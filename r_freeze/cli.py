14eb8ecf59af38f197e6098f49bcb92114bd9f6d
Chinmay Shah <chinmayshah3899@gmail.com>
2020-10-03 19:42:37 +0530

    fixed module import

--- r_freeze/cli.py
+++ r_freeze/cli.py
@@ -1,14 +1,22 @@
 """Console script for r_freeze."""
 import argparse
 import sys
-from r_freeze import get_packages, write_package_file
+from r_freeze.r_freeze import get_packages, write_package_file
+
 
 def main():
     """Console script for r_freeze."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir', type=str, help="Directory to look for")
-    parser.add_argument('-o', type=str, help="Name of outfile if you want to craete one")
-    parser.add_argument('--overwrite', action='store_true' ,default=False, help="Over write output file if it already exists")
+    parser.add_argument("dir", type=str, help="Directory to look for")
+    parser.add_argument(
+        "-o", type=str, help="Name of outfile if you want to craete one"
+    )
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        default=False,
+        help="Over write output file if it already exists",
+    )
 
     args = parser.parse_args()
     if not isinstance(args.dir, str):
